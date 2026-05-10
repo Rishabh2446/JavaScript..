e947e75aa67b304281d2f3528d4ac23c64baba5d
Rishabh Gupta <guptarishabh8686@gmail.com>
2026-05-10 16:36:16 +0530

    Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we use node js not browser ,it works on browser
+
+console.log(3+3) 
+// code readability should be high
+console.log("rishu")
+
+let name = "Rishu" // string
+let age = 17 // number
+let isLoggedIn = false // boolean
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => 
+
+// object
+
+console.log((typeof ""))
+console.log((typeof age))
+console.log((typeof null)) // obj
+console.log((typeof undefined)) // undefined
