d5778b17c3ebd183d8ed645c2bc89830fb59c067
Rishabh Gupta <guptarishabh8686@gmail.com>
2026-05-10 16:54:38 +0530

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+// "33avc" => NAN 
+// true => 1; false => 0
+
+let isLoggedIn = "rishu"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true; 0 => false;
+// "" => false
+// "name" => true
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
