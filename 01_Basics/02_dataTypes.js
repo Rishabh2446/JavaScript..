bdfcac7ca9cb8e5d198df8c324bd041519e91e13
Rishabh Gupta <guptarishabh8686@gmail.com>
2026-05-11 11:11:35 +0530

    adding datatype summary

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -24,3 +24,48 @@ console.log((typeof ""))
 console.log((typeof age))
 console.log((typeof null)) // obj
 console.log((typeof undefined)) // undefined
+
+// datatypes summary ->
+
+// primitive(7)->
+/*
+1. String 
+2. Number
+3. Boolean
+4. null
+5. undefined
+6. symbol
+7.BigInt
+*/
+
+const score = 100;
+const scoreValue = 100.3
+// const isLoggedIn = false;
+const outtemp = null;
+let userEmail =  undefined;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 23455656868349092390n
+
+//Reference Type (Non-Primitive)
+/*
+1. Array
+2. Objects
+3. Functions
+*/
+
+const heros = ["shaktiman", "nagraj", "doga"]
+let myObj = {
+    name: "rishu",
+    age: 24,
+}
+
+const myFunc = function(){
+    console.log("Hello");
+}
+
+console.log(typeof bigNumber);
